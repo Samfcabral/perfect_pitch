f4f2f165539c34bf02917d4f6c3164a3ce2004c4
Samuel Cabral <samfcabral@gmail.com>
2015-02-15 15:26:42 -0800

    basic setup of notes

--- app.js
+++ app.js
@@ -188,34 +188,10 @@ for (var i = 0; i < level; i++) {
   };
 }
 
-
-// CLICK ON "PLAY NOTE" BUTTON ON HOME PAGE
-  // On click ("play")
-    // hide "play note" button
-    // check what level user on
-    // display note options (setup notes function maybe?)
-    // randomly play a note (audibly based on user level)
-    // wait for user click
-
-  // On click of a note
-    //check to see if it matches "play note"
-    // if yes
-      // update scoreboard (# of accurate consecutive guesses)
-        // if applicable update user level
-      // display "correct" message (ideally an audio file)
-      // display "play note" button
-    // if no
-      // scoreboard = 0
-      // display "incorrect" message (audio file)
-      // display "play note" button
-
-
-
-
-// db.sequelize.sync().then(function() {
-//   var server = app.listen(process.env.PORT || 3000, function() {
-//     console.log(new Array(51).join("*"));
-//     console.log("\t LISTENING ON: \n\t\t localhost:3000");
-//     console.log(new Array(51).join("*")); 
-//   });
-// });
+db.sequelize.sync().then(function() {
+  var server = app.listen(process.env.PORT || 3000, function() {
+    console.log(new Array(51).join("*"));
+    console.log("\t LISTENING ON: \n\t\t localhost:3000");
+    console.log(new Array(51).join("*")); 
+  });
+});
