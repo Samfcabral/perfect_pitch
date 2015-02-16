35728bde61dc87942f2cd0bbdbdd5453042facd3
Samuel Cabral <samfcabral@gmail.com>
2015-02-15 22:35:59 -0800

    notes working

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -25,34 +25,32 @@ function shuffle(array) {
   return array;
 }
 
-// function for when user clicks "play"
+// when user clicks "play"
 $(function () {
-
-
-// setting up all possible divs such that divs are called divs[0] through divs[11]
-for (var i = 1; i < 12; i++) {
-  divs.push(document.getElementById('note' + i));
-}
-
-// function to check if note played by computer matches note played by user
-var note_match = function (note_number, note_clicked) {
-  if (note_number === note_clicked) {
-    //update score
-      //if applicable update level
-    //show "correct note" message
-  } else {
-    //update score to zero
-    //show "incorrect note" message
+  // setting up divs
+  for (var i = 1; i < 12; i++) {
+    divs.push(document.getElementById('note' + i));
   }
-  setup_notes(level);
-};
 
-// looping through to set click handlers 
-for (var i = 0; i < level; i++) {
-  divs[i].onclick = function(event) {
-    note_match(note_number, my_notes_array[i]);
+  // function to check if note played by computer matches note played by user
+  var note_match = function (note_number, note_clicked) {
+    if (note_number === note_clicked) {
+      //update score
+        //if applicable update level
+      //show "correct note" message
+    } else {
+      //update score to zero
+      //show "incorrect note" message
+    }
+    setup_notes(level);
   };
-}
+
+  // looping through to set click handlers 
+  for (var i = 0; i < level; i++) {
+    divs[i].onclick = function(event) {
+      note_match(note_number, my_notes_array[i]);
+    };
+  }
 
   // function to set up images
   var set_notes = function (level) {
@@ -62,23 +60,28 @@ for (var i = 0; i < level; i++) {
     //set inner HTML
     shuffle(my_notes_array);
     my_notes_array.forEach(function (note, i) {
-divs[i].innerHTML = "<img src='images/" + (my_notes_array[i] +1) + ".png'>";
-    });
+    divs[i].innerHTML = "<img src='images/" + (my_notes_array[i] +1) + ".png'>";
+        });
   }
+
   set_notes(level);
+  
   var x = document.createElement("audio"); 
-  console.log("Running")
+  console.log("Running");
   $(".play").on("click", function () {
-      console.log("clicked")
+      console.log("clicked");
       var note_played = Math.ceil(Math.random() * level);
       console.log(note_played);
-     // var x = document.createElement("audio"); 
-     console.log(x.ended)
+       // var x = document.createElement("audio"); 
+       console.log(x.ended)
       x.src = "audio/" + note_played +".mp3"; 
       x.play();
   })
 });
 
-
-
-
+  // var y = document.createElement("clicked_note");
+  // console.log("working");
+  //   $(".notes").("click", function () {
+  //     console.log("note clicked");
+  //   })
+  // });
